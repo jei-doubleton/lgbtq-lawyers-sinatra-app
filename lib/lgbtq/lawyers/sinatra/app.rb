2c6fb7e1695d44271b320ea1bfe2f11889f52883
J. Singleton <jennifer.singleton@gmail.com>
2019-05-09 20:14:27 +0000

    first commit

---
+++ lib/lgbtq/lawyers/sinatra/app.rb
@@ -0,0 +1,12 @@
+require "lgbtq/lawyers/sinatra/app/version"
+
+module Lgbtq
+  module Lawyers
+    module Sinatra
+      module App
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/lgbtq/lawyers/sinatra/app/version.rb
@@ -0,0 +1,9 @@
+module Lgbtq
+  module Lawyers
+    module Sinatra
+      module App
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
