b435784c4390cfa79af84dfa2ff3671f1d8a621a
J. Singleton <jennifer.singleton@gmail.com>
2019-05-15 02:28:53 +0000

    add validation to check if username or email has been used, with accompanying flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,18 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:user][:email] == "" || params[:user][:username] == "" || params[:user][:password] == ""
-      flash[:message] = "You must fill in all fields!"
+      flash[:message] = "You must complete all fields."
+      redirect "/signup"
+    elsif User.all.find_by(username: params[:user][:username])
+      flash[:message] = "The username '#{params[:user][:username]}' is already taken."
+      redirect "/signup"
+    elsif User.all.find_by(email: params[:user][:email])
+      flash[:message] = "The email address '#{params[:user][:email]}' was already used to create an account. If you already have an account, login using the link above."
       redirect "/signup"
     else
       @user = User.create(params[:user])
       session[:user_id] = @user.id
+      binding.pry
       erb :"users/show"
     end
   end
