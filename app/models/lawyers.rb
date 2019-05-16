d927145afc4d794079ab4dcae5ea8214f3bdce1f
J. Singleton <jennifer.singleton@gmail.com>
2019-05-16 01:01:45 +0000

    create lawyer slug

--- app/models/lawyers.rb
+++ app/models/lawyers.rb
@@ -2,4 +2,8 @@ class Lawyer < ActiveRecord::Base
   belongs_to :user
   has_many :lawyer_practice_areas
   has_many :practice_areas, through: :lawyer_practice_areas
+
+  def slug
+    self.name.downcase.gsub(" ", "-").gsub(/[!|@|#|$|%|^|&|*|.]/, "")
+  end
 end
