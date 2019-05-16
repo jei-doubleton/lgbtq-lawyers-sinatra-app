c249635d12107f8ccdafdb8324918798657eeb37
J. Singleton <jennifer.singleton@gmail.com>
2019-05-16 00:19:40 +0000

    add create lawyer form

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,3 +1,17 @@
+require 'rack-flash'
 class LawyersController < ApplicationController
+  use Rack::Flash
 
+  get '/lawyers/new' do
+    if logged_in?
+      erb :"/lawyers/new"
+    else
+      flash[:message] = "You must be logged in to view that page."
+      redirect '/login'
+    end
+  end
+
+  post '/lawyers' do
+    Lawyer.create()
+  end
 end
