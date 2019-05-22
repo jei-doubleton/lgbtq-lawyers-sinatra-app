fd826605d66769b4adcbd640c00738abdb5a174e
J. Singleton <jennifer.singleton@gmail.com>
2019-05-22 20:51:26 +0000

    add PracticeArea alphabetization method to PracticeArea model

--- app/models/practice_areas.rb
+++ app/models/practice_areas.rb
@@ -1,4 +1,8 @@
 class PracticeArea < ActiveRecord::Base
   has_many :lawyer_practice_areas
   has_many :lawyers, through: :lawyer_practice_areas
+
+  def self.alpha
+    self.all.sort {|a, b| a.name.capitalize <=> b.name.capitalize}
+  end
 end
